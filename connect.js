9efde54cd65d3c409cfa0daab4a7ff0b0d6d1f74
lehiephung <lehiephung199@gmail.com>
2025-08-05 21:21:54 +0700

    update

--- connect.js
+++ connect.js
@@ -1,12 +1,13 @@
 const mysql2 = require('mysql2');
-const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connection = mysql2.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'provinces_db'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = connection.promise();
