d676c5ba128fa6be1be2b561eeae2721353ce372
lehiephung <lehiephung199@gmail.com>
2025-08-07 21:26:43 +0700

    update

--- connect.js
+++ connect.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql2.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect(err => {
